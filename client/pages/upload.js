3c723f50f762d7da89215a515d43855949b4c8d5
Matthias <matthias@rouen-webmaster.com>
2023-06-02 22:48:14 +0200

    feature: redirect

--- client/pages/upload.js
+++ client/pages/upload.js
@@ -2,7 +2,11 @@ import Header from '../components/Header/HeaderComponent';
 import UploadImage from '../components/UploadComponent/UploadComponent';
 
 export default function ImageUpload() {
-
+  useEffect(() => {
+    if (!localStorage.getItem('authToken')) {
+      window.location = '/login';
+    }
+  }, []);
   return (
     <>
     <Header />
