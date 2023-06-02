24ccc09dc8187d862c9800ea1eb78a01df17c7b1
Matthias <matthias@rouen-webmaster.com>
2023-06-02 22:50:51 +0200

    fix

--- client/pages/upload.js
+++ client/pages/upload.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header/HeaderComponent';
 import UploadImage from '../components/UploadComponent/UploadComponent';
+import { useEffect } from 'react';
 
 export default function ImageUpload() {
   useEffect(() => {
