e50022df4a3b530bd8e35190a4d6cbc84fb3842a
Matthias <matthias@rouen-webmaster.com>
2023-06-06 14:27:16 +0200

    test

--- client/components/LoginComponent/LoginComponent.js
+++ client/components/LoginComponent/LoginComponent.js
@@ -5,7 +5,6 @@ import { removeUserIdFromCookie, saveUserIdToCookie } from '../../utils/auth';
 import { FormControl, OutlinedInput, Button, TextField, Alert } from '@mui/material';
 import { serverAdress } from '../../services/utils';
 
-
 export default function LoginComponent() {
   const [error, setError] = useState('');
 
